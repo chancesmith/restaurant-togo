ec85c0a8f837ec6cf336da5df0ff7ce37923eb28
Chance Smith <chancesmithb@gmail.com>
2020-03-25 21:35:57 -0500

    📦  NEW: try new config

--- src/components/StoresRoll.js
+++ src/components/StoresRoll.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 import { Link, graphql, StaticQuery } from "gatsby";
 import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
-class BlogRoll extends React.Component {
+class StoresRoll extends React.Component {
   render() {
     const { data } = this.props;
     const { edges: posts } = data.allMarkdownRemark;
 
     return (
       <div className="columns is-multiline">
+        {console.log({ posts })}
         {posts &&
           posts.map(({ node: post }) => (
             <div className="is-parent column is-6" key={post.id}>
@@ -17,26 +18,16 @@ class BlogRoll extends React.Component {
                 className={`blog-list-item tile is-child box notification`}
               >
                 <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`
-                        }}
-                      />
-                    </div>
-                  ) : null}
                   <p className="post-meta">
                     <Link
                       className="title has-text-primary is-size-4"
                       to={post.fields.slug}
                     >
-                      {post.frontmatter.title}
+                      {post.frontmatter.restaurantName}
                     </Link>
                     <span> &bull; </span>
                     <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
+                      {post.frontmatter.tags.join(", ")}
                     </span>
                   </p>
                 </header>
@@ -45,7 +36,7 @@ class BlogRoll extends React.Component {
                   <br />
                   <br />
                   <Link className="button" to={post.fields.slug}>
-                    Keep Reading →
+                    View Details →
                   </Link>
                 </p>
               </article>
@@ -56,7 +47,7 @@ class BlogRoll extends React.Component {
   }
 }
 
-BlogRoll.propTypes = {
+StoresRoll.propTypes = {
   data: PropTypes.shape({
     allMarkdownRemark: PropTypes.shape({
       edges: PropTypes.array
@@ -67,10 +58,10 @@ BlogRoll.propTypes = {
 export default () => (
   <StaticQuery
     query={graphql`
-      query BlogRollQuery {
+      query StoresRollQuery {
         allMarkdownRemark(
           sort: { order: DESC, fields: [frontmatter___date] }
-          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+          filter: { frontmatter: { templateKey: { eq: "store-listing" } } }
         ) {
           edges {
             node {
@@ -80,15 +71,15 @@ export default () => (
                 slug
               }
               frontmatter {
-                title
-                templateKey
+                restaurantName
                 date(formatString: "MMMM DD, YYYY")
+                tags
               }
             }
           }
         }
       }
     `}
-    render={(data, count) => <BlogRoll data={data} count={count} />}
+    render={(data, count) => <StoresRoll data={data} count={count} />}
   />
 );
